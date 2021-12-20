fbeb4c8a5e1aa522658652415b9e84d23080cc4c
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2021-12-20 00:43:18 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+update-venvs
+============
+
+A simple script to update all my poetry-based projects whenever a breaking change occurs that breaks all my venvs.
+
+I wouldn't recommend using it unless you are sure it works for you as well.
---
+++ update_venvs.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+import subprocess
+from contextlib import contextmanager
+from pathlib import Path
+
+from tomlkit import parse
+from tomlkit.toml_document import TOMLDocument
+
+
+@contextmanager
+def change_directory(path: Path) -> None:
+    origin = Path().absolute()
+
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(origin)
+
+
+def get_extras(pyproject: TOMLDocument) -> list[str]:
+    tool = pyproject.get('tool', {})
+    poetry = tool.get('poetry', {})
+    extras = poetry.get('extras', {})
+    return list(extras.keys())
+
+
+def update_env(pyproject_path: Path) -> None:
+    project_path = env.parent
+
+    venv = project_path / '.venv'
+
+    with pyproject_path.open() as pyproject_file:
+        pyproject = parse(pyproject_file.read())
+        extras = get_extras(pyproject)
+
+    env = os.environ.copy()
+    del env["VIRTUAL_ENV"]
+
+    with change_directory(project_path):
+        shutil.rmtree(venv, ignore_errors=True)
+
+        subprocess.run(['poetryup'], env=env)
+
+        subprocess.run(['poetry', 'update'], env=env)
+
+        extras_args = [arg for extra in extras for arg in ['-E', extra]] if extras else []
+        subprocess.run(['poetry', 'install', *extras_args], env=env)
+
+
+def update_envs(workspace: Path) -> None:
+    pyproject_tomls = workspace.glob('**/pyproject.toml')
+
+    for pyproject_path in pyproject_tomls:
+        update_env(pyproject_toml)
+
+
+if __name__ == '__main__':
+    update_envs(Path('..'))
